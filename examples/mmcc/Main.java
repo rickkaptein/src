8136451529e884bce0708f94abe90f497407ea55
rickkaptein <rickkaptein@gmail.com>
2018-04-07 14:17:35 +0200

    weights and seats

--- examples/mmcc/Main.java
+++ examples/mmcc/Main.java
@@ -13,6 +13,12 @@ public class Main {
 		double timeHorizon = 179;
 		long n = 1000;
 		long seed = 0;
+		int[][] weights = {{11, 15, 18, 20, 19, 15, 12, 11, 13, 8},
+							{8, 9, 11, 12, 14, 15, 16, 18, 20, 8}, 
+							{1, 5, 8, 10, 11, 12, 13, 15, 20, 8}};
+		int[] seats = {20,20,20,20,20,20,20,20,20,Integer.MAX_VALUE};
+		
+		
 		
 		MMCCState state = new MMCCState(lambda, timeHorizon, seed);
 		Replication<MMCCState> replication = new AutoReplication<>(state);
